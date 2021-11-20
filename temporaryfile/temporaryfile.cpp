e0dc64b4e84bf7be6438b4483031ac973a00965e
kevin <kevinchannon@gmail.com>
2021-11-20 07:50:51 +0000

    Add project files.

---
+++ temporaryfile/temporaryfile.cpp
@@ -0,0 +1,16 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace temporaryfile
+{
+	TEST_CLASS(TestTemporaryFile)
+	{
+	public:
+		
+		TEST_METHOD(CreateTempFilePath)
+		{
+
+		}
+	};
+}
