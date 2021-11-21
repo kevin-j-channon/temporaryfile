cfaac481708eccec84961be3aad982120ab51c10
kevin <kevinchannon@gmail.com>
2021-11-21 08:16:39 +0000

    Removing TemporaryPath classes and tests

--- temporaryfile/temporary_file.hpp
+++ temporaryfile/temporary_file.hpp
@@ -1,16 +1,6 @@
 #pragma once
 
 #include <filesystem>
-#include <string>
-#include <fstream>
-#include <memory>
-#include <cassert>
-
-enum class path_type
-{
-	file,
-	directory
-};
 
 
 std::filesystem::path create_tmp_path(const std::filesystem::path& rel_path)
@@ -40,69 +30,24 @@ private:
 	std::filesystem::path m_path;
 };
 
-template<path_type PATH_TYPE>
-class TemporaryPath
+class ScopedDirectoryDeleter
 {
 public:
-	explicit TemporaryPath(const std::filesystem::path& rel_path) {
-		m_path = std::shared_ptr<std::filesystem::path>(new std::filesystem::path{ std::filesystem::temp_directory_path() / rel_path },
-			[this](auto* p) {
-				std::error_code _;
-				std::filesystem::remove_all(this->m_root_dir, _);
+	explicit ScopedDirectoryDeleter(std::filesystem::path path)
+		: m_path{ std::move(path) }
+	{}
 
-				delete p;
-			});
+	ScopedDirectoryDeleter(const ScopedDirectoryDeleter&) = delete;
+	ScopedDirectoryDeleter& operator=(const ScopedDirectoryDeleter&) = delete;
 
-		m_root_dir = _calculate_first_parent_directory(rel_path);
-
-		assert(m_path);
-	}
-
-	const auto& path() const {
-		assert(m_path);
-		return *m_path;
-	}
-
-	TemporaryPath& create() {
-		assert(m_path);
-
-		if constexpr (PATH_TYPE == path_type::file) {
-			if (!std::filesystem::exists(m_path->parent_path())) {
-				std::filesystem::create_directories(m_path->parent_path());
-			}
-
-			std::ofstream _(m_path->string());
+	~ScopedDirectoryDeleter()
+	{
+		if (std::filesystem::exists(m_path))
+		{
+			std::filesystem::remove_all(m_path);
 		}
-		else if constexpr (PATH_TYPE == path_type::directory) {
-			std::filesystem::create_directories(*m_path);
-		}
-		else {
-			static_assert(false, "Invalid path type");
-		}
-
-		return *this;
 	}
 
 private:
-
-	static std::filesystem::path _calculate_first_parent_directory(std::filesystem::path p) {
-		auto first_parent = std::filesystem::path{};
-		while (true) {
-			p = p.parent_path();
-			if (!p.string().empty()) {
-				first_parent = p;
-			}
-			else {
-				break;
-			}
-		}
-
-		return first_parent;
-	}
-
-	std::filesystem::path m_root_dir;
-	std::shared_ptr<std::filesystem::path> m_path;
+	std::filesystem::path m_path;
 };
-
-using TemporaryFile = TemporaryPath<path_type::file>;
-using TemporaryDirectory = TemporaryPath<path_type::directory>;
--- temporaryfile/temporaryfile.cpp
+++ temporaryfile/temporaryfile.cpp
@@ -3,7 +3,7 @@
 #include "CppUnitTest.h"
 
 #include <filesystem>
-#include <format>
+#include <fstream>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -11,17 +11,17 @@ namespace fs = std::filesystem;
 
 namespace temporaryfile
 {
-	TEST_CLASS(TestTemporaryFile)
+	TEST_CLASS(FreeFunctions)
 	{
 	public:
 
-		TEST_METHOD(CreateTempFilePath1)
+		TEST_METHOD(CreateTempFilePath)
 		{
 			const auto path = create_tmp_path("foo.txt");
 			Assert::IsTrue(path.string().ends_with("foo.txt"));
 		}
 
-		TEST_METHOD(CreateScopedFileDeleter)
+		TEST_METHOD(ScopedFileDeleterDeletesOnScopeExit)
 		{
 			const auto path = create_tmp_path("foo.txt");
 			{
@@ -33,83 +33,19 @@ namespace temporaryfile
 			Assert::IsFalse(fs::exists(path));
 		}
 
-		TEST_METHOD(CreateTempFilePath)
+		TEST_METHOD(ScopedDirDeleterDeletesOnScopeExit)
 		{
-			const auto temp_file = TemporaryFile("foo.txt");
-
-			Assert::AreEqual(std::string("foo.txt"), temp_file.path().filename().string());
-		}
-
-		TEST_METHOD(CreateTempFile)
-		{
-			const auto temp_file = TemporaryFile("foo.txt").create();
-			Assert::IsTrue(fs::exists(temp_file.path()));
-		}
-
-		TEST_METHOD(CreateTempFileInSubdirectory)
-		{
-			const auto temp_file = TemporaryFile(fs::path{ "foo" } / "bar" / "baz.txt").create();
-			Assert::IsTrue(fs::exists(temp_file.path()));
-		}
-
-		BEGIN_TEST_METHOD_ATTRIBUTE(TempFileIsDeletedWhenScopeEnds)
-			TEST_IGNORE()
-			END_TEST_METHOD_ATTRIBUTE()
-		TEST_METHOD(TempFileIsDeletedWhenScopeEnds)
-		{
-			auto path = fs::path{};
-
+			const auto path = create_tmp_path("foo");
 			{
-				const auto temp_file = TemporaryFile("foo.txt").create();
-				path = temp_file.path();
+				auto _ = ScopedDirectoryDeleter(path);
+
+				fs::create_directories(path);
+
+				Assert::IsTrue(fs::exists(path));
+				Assert::IsTrue(fs::is_directory(path));
 			}
 
 			Assert::IsFalse(fs::exists(path));
-		}
-
-		BEGIN_TEST_METHOD_ATTRIBUTE(AllCreatedSubdirectoriesAreDeleteWhenScopeEnds)
-			TEST_IGNORE()
-		END_TEST_METHOD_ATTRIBUTE()
-		TEST_METHOD(AllCreatedSubdirectoriesAreDeleteWhenScopeEnds)
-		{
-			auto path = fs::path{};
-			{
-				const auto temp_file = TemporaryFile(fs::path{ "foo" } / "bar" / "baz.txt").create();
-				path = temp_file.path();
-			}
-
-			Assert::IsFalse(fs::exists(path));
-			Assert::IsFalse(fs::exists(path.parent_path()));
-			Assert::IsFalse(fs::exists(path.parent_path().parent_path()));
-			Assert::IsTrue(fs::exists(path.parent_path().parent_path().parent_path()));
-		}
-
-		TEST_METHOD(CopyingTempFilesDoesntDeleteTheFilePrematurely)
-		{
-			const auto temp_1 = TemporaryFile("foo.txt").create();
-
-			{
-				const auto temp_2 = temp_1;
-			}
-
-			Assert::IsTrue(fs::exists(temp_1.path()));
-		}
-	};
-
-	TEST_CLASS(TestTemporaryDirectory)
-	{
-		TEST_METHOD(CreateTempDirPath)
-		{
-			const auto temp_dir = TemporaryDirectory("foo");
-			Assert::AreEqual(std::string("foo"), temp_dir.path().filename().string());
-		}
-
-		TEST_METHOD(CreateTempDir)
-		{
-			const auto temp_dir = TemporaryDirectory("foo").create();
-			Assert::IsTrue(fs::exists(temp_dir.path()));
-			Assert::IsTrue(fs::is_directory(temp_dir.path()));
-			Assert::IsFalse(fs::is_regular_file(temp_dir.path()));
 		}
 	};
 }
