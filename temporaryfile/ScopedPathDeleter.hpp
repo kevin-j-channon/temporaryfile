2d346e00fea8c16eb8df8e4263d5a8e55e087900
kevin <kevinchannon@gmail.com>
2021-11-22 07:24:53 +0000

    Adding version that runs via a generic RAII class and some macros

--- temporaryfile/ScopedPathDeleter.hpp
+++ temporaryfile/ScopedPathDeleter.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <filesystem>
+#include <type_traits>
+#include <functional>
 
 class ScopedPathDeleter
 {
@@ -23,3 +25,37 @@ public :
 private:
 	std::filesystem::path m_path;
 };
+
+template<typename Fn_T>
+struct ScopeExitRunner
+{
+	explicit ScopeExitRunner(Fn_T&& fn) : m_fn{ std::move(fn) } {}
+	
+	ScopeExitRunner(const ScopeExitRunner&) = delete;
+	ScopeExitRunner& operator=(const ScopeExitRunner&) = delete;
+
+	~ScopeExitRunner() { m_fn(); }
+	
+	Fn_T m_fn;
+};
+
+template<typename T>
+ScopeExitRunner<T> make_scope_exit_runner(T&& fn) {
+	return ScopeExitRunner<T>{std::forward<T>(fn)};
+}
+
+#define SCOPED_PATH_REMOVER(the_path) \
+	make_scope_exit_runner(\
+		[&the_path](){ \
+			if (std::filesystem::exists(the_path))\
+				std::filesystem::remove_all(the_path);\
+		} \
+	)
+
+#define AUTO_REMOVE_PATH(the_path) \
+	const auto __auto_path_remover##__LINE__##__ = make_scope_exit_runner(\
+		[&the_path](){ \
+			if (std::filesystem::exists(the_path))\
+				std::filesystem::remove_all(the_path);\
+		} \
+	)
--- temporaryfile/UnitTest.cpp
+++ temporaryfile/UnitTest.cpp
@@ -59,4 +59,69 @@ namespace test_path_deleter
 			Assert::IsTrue(fs::exists(path.parent_path().parent_path().parent_path()));
 		}
 	};
+
+	TEST_CLASS(MacroTests)
+	{
+	public:
+
+		TEST_METHOD(ScopedFileDeleterDeletesOnScopeExit)
+		{
+			const auto path = fs::temp_directory_path() / "foo.txt";
+			{
+				auto _ = SCOPED_PATH_REMOVER(path);
+
+				{ std::ifstream(path.string().c_str()); }
+			}
+
+			Assert::IsFalse(fs::exists(path));
+		}
+
+		TEST_METHOD(ScopedDirDeleterDeletesOnScopeExit)
+		{
+			const auto path = fs::temp_directory_path() / "foo";
+			{
+				auto _ = SCOPED_PATH_REMOVER(path);
+
+				fs::create_directories(path);
+
+				Assert::IsTrue(fs::exists(path));
+				Assert::IsTrue(fs::is_directory(path));
+			}
+
+			Assert::IsFalse(fs::exists(path));
+		}
+
+		TEST_METHOD(AutoPathRemoverMacro)
+		{
+			const auto path = fs::temp_directory_path() / "foo";
+			{
+				AUTO_REMOVE_PATH(path);
+
+				fs::create_directories(path);
+
+				Assert::IsTrue(fs::exists(path));
+				Assert::IsTrue(fs::is_directory(path));
+			}
+
+			Assert::IsFalse(fs::exists(path));
+		}
+
+		TEST_METHOD(NestedDirectoriesAreRemovedOnScopeExit)
+		{
+			const auto path = fs::temp_directory_path() / "foo" / "bar" / "baz1.txt";
+			{
+				auto _ = ScopedPathDeleter{ fs::temp_directory_path() / "foo" };
+
+				fs::create_directories(path.parent_path());
+				std::ofstream f(path.string().c_str());
+
+				Assert::IsTrue(fs::exists(path));
+			}
+
+			Assert::IsFalse(fs::exists(path));
+			Assert::IsFalse(fs::exists(path.parent_path()));
+			Assert::IsFalse(fs::exists(path.parent_path().parent_path()));
+			Assert::IsTrue(fs::exists(path.parent_path().parent_path().parent_path()));
+		}
+	};
 }
